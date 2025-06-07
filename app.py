542726d4833f3991c73a346106711243d6ec33c2
lucky071118 <lucky071118@gmail.com>
2025-06-07 07:41:57 +0000

    use agent with conversation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, abort
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import (
@@ -6,32 +6,32 @@ from linebot.v3.messaging import (
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
-    TextMessage
+    TextMessage,
 )
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
 from mistralai import Mistral
 import os
 
 app = Flask(__name__)
 
-LINE_CHANNEL_SECRET = os.environ['LINE_CHANNEL_SECRET']
-LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
-MISTRAL_API_KEY = os.environ['MISTRAL_API_KEY']
+
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+MISTRAL_API_KEY = os.environ["MISTRAL_API_KEY"]
+MISTRAL_AGENT_ID = os.environ["MISTRAL_AGENT_ID"]
+
 configuration = Configuration(access_token=LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
 
 @app.route("/", methods=["GET", "HEAD"])
 def health_check():
     return "OK", 200
 
-@app.route("/callback", methods=['POST'])
+
+@app.route("/callback", methods=["POST"])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers["X-Line-Signature"]
 
     # get request body as text
     body = request.get_data(as_text=True)
@@ -41,32 +41,41 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        app.logger.info(
+            "Invalid signature. Please check your channel access token/channel secret."
+        )
         abort(400)
 
-    return 'OK'
+    return "OK"
+
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with (
         Mistral(api_key=MISTRAL_API_KEY) as mistral,
-        ApiClient(configuration) as api_client
+        ApiClient(configuration) as api_client,
     ):
-        res = mistral.chat.complete(model="mistral-large-latest", messages=[
-            {
-                "content": event.message.text,
-                "role": "user",
-            },
-        ])
+        response = mistral.beta.conversations.append(conversation_id=conversation_id, inputs=event.message.text)
 
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=res.choices[0].message.content)]
+                messages=[TextMessage(text=response.outputs[0].content)],
             )
         )
 
-if __name__ == '__main__':
+
+def create_conversation():
+    with Mistral(
+        api_key=os.getenv("MISTRAL_API_KEY", ""),
+    ) as mistral:
+        response = mistral.beta.conversations.start(agent_id=MISTRAL_AGENT_ID, inputs="你好")
+        return response.conversation_id
+
+conversation_id = create_conversation()
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
+
